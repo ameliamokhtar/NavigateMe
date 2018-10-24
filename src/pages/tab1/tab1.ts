757c8586164a9af972c45b17c6a9ae4e23e791c0
Roslan Logan <roslanlogan94@gmail.com>
2018-10-24 16:31:31 +0800

    adsqwd

--- src/pages/tab1/tab1.ts
+++ src/pages/tab1/tab1.ts
@@ -16,7 +16,7 @@ import { EstimoteBeacons } from "@ionic-native/estimote-beacons";
 import { ServiceProvider } from "../../providers/service/service";
 
 // plugins
-import { IBeacon } from "ionic-native";
+import { IBeacon } from "@ionic-native/ibeacon";
 
 // providers
 import { BeaconProvider } from "../../providers/beacon/beacon-provider";
@@ -26,6 +26,7 @@ import { BeaconModel } from "../../module/beacon-module";
 
 declare var evothings: any;
 declare var google;
+declare var IndoorAtlas: any;
 @IonicPage()
 @Component({
   selector: "page-tab1",
@@ -44,8 +45,9 @@ export class Tab1Page {
   selectedLocation: any;
   uuid: any;
   distance: any;
-  lowpass:any;
-  beaconList=[];
+  lowpass: any;
+  beaconList = [];
+  region: any;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -54,40 +56,117 @@ export class Tab1Page {
     private eb: EstimoteBeacons,
     private service: ServiceProvider,
     public beaconProvider: BeaconProvider,
-    public events: Events
+    public events: Events,
+    public ibeacon: IBeacon
   ) {}
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad Tab1Page");
     this.getOfficeData();
     // this.startScanningForBeacons();
+    // setup a beacon region – CHANGE THIS TO YOUR OWN UUID
+    this.region = this.ibeacon.BeaconRegion(
+      "bbd2b66fac64f79c93bbe028e776fa3c",
+      "B9407F30-F5F8-466E-AFF9-25556B57FE6D"
+    );
+
+   
+    // this.eb.startMonitoringForRegion(this.region,true).subscribe(data=>{
+    //   console.log(data);
+    // })
+    // this.ibeacon.startRangingBeaconsInRegion(this.region).then(
+    //   (data) => {
+    //     // resolve(true);
+    //     console.log(data);
+    //   },
+    //   error => {
+    //     console.error("Failed to begin monitoring: ", error);
+    //     // resolve(false);
+    //   }
+    // );
+  }
+  startLocating(){
+     // start ranging
+     this.eb.startRangingBeaconsInRegion(this.region).subscribe(data => {
+      console.log(data);
+    });
+    // onSuccess Callback
+    var onSuccess = function() {
+      alert("IndoorAtlas was successfully initialized");
+      IndoorAtlas.getCurrentPosition(onSuccess2, onError2);
+    };
+
+    // onError Callback receives a PositionError object
+    function onError(error) {
+      alert("Code: " + error.code + "\n" + "Message: " + error.message);
+    }
+
+    IndoorAtlas.initialize(onSuccess, onError, {
+      key: "85385ec6-8e0c-45e7-af01-776d25ae6801",
+      secret:
+        "eHE3bzJ4tzIjUES00puoyoZlSlZ1T74ENNxJuOxlk/U5x5tm7IQs7E923BrUUtmVx9s73YK8cwiBwlNSkyrqIg4sYtN3yILJuIsJSMQ0QjqLd0+xhjPebDsH8EWDKw=="
+    });
+    console.log(IndoorAtlas);
+    // onSuccess Callback
+    // This method accepts a Position object, which contains the
+    // current GPS coordinates
+    var onSuccess2 = function(position) {
+      console.log(position)
+      alert(
+        "Latitude: " +
+          position.coords.latitude +
+          "\n" +
+          "Longitude: " +
+          position.coords.longitude +
+          "\n" +
+          "Altitude: " +
+          position.coords.altitude +
+          "\n" +
+          "Accuracy: " +
+          position.coords.accuracy +
+          "\n" +
+          "Heading: " +
+          position.coords.heading +
+          "\n" +
+          "Floor: " +
+          position.coords.floor +
+          "\n" +
+          "Timestamp: " +
+          position.timestamp
+      );
+    };
+
+    // onError Callback receives a PositionError object
+    function onError2(error) {
+      alert("Code: " + error.code + "\n" + "Message: " + error.message);
+    }
+
     
   }
-
   startScanningForBeacons() {
-      evothings.eddystone.startScan(
-        data => {
-          this.beaconData = data;
-          if (
-            this.beaconData &&
-            this.beaconData.txPower &&
+    evothings.eddystone.startScan(
+      data => {
+        this.beaconData = data;
+        if (
+          this.beaconData &&
+          this.beaconData.txPower &&
+          this.beaconData.rssi
+        ) {
+          this.distance = evothings.eddystone.calculateAccuracy(
+            this.beaconData.txPower,
             this.beaconData.rssi
-          ) {
-            this.distance = evothings.eddystone.calculateAccuracy(
-              this.beaconData.txPower,
-              this.beaconData.rssi
-            );
-            this.beaconData.distance = this.distance + " Meter";
-          }
-          this.lowpass = evothings.eddystone.createLowPassFilter(0.8)
-          this.beaconData.distancefiltered = this.lowpass.filter(this.distance);
-          console.log(this.beaconData);
-          this.beaconList.push(this.beaconData)
-         },
-        error => console.error(error)
-      );
+          );
+          this.beaconData.distance = this.distance + " Meter";
+        }
+        this.lowpass = evothings.eddystone.createLowPassFilter(0.8);
+        this.beaconData.distancefiltered = this.lowpass.filter(this.distance);
+        console.log(this.beaconData);
+        this.beaconList.push(this.beaconData);
+      },
+      error => console.error(error)
+    );
   }
-  stop(){
+  stop() {
     evothings.eddystone.stopScan();
   }
   getOfficeData() {
@@ -99,7 +178,7 @@ export class Tab1Page {
       }
     });
   }
-  clear(){
+  clear() {
     this.beaconList = [];
   }
   selectLocation(location) {
